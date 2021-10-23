88ad5ae5a94319c27f498e436fedc4b54161b984
Rifki <rifkipelajar@gmail.com>
2021-10-24 00:13:07 +0800

    UseCase Watchlist Tv Shows

---
+++ lib/domain/usecases/get_watchlist_tv_shows.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/repositories/tv_show_repository.dart';
+
+class GetWatchlistTvShows {
+  final TvShowRepository _repository;
+
+  GetWatchlistTvShows(this._repository);
+
+  Future<Either<Failure, List<TvShow>>> execute() {
+    return _repository.getWatchlistTvShows();
+  }
+}
