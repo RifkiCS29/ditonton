95cadacd41945e1800377516f3bbe03b0250b622
Rifki <rifkipelajar@gmail.com>
2021-10-24 00:06:32 +0800

    UseCase Tv Show Recommendations

---
+++ lib/domain/usecases/get_tv_show_recommendations.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/repositories/tv_show_repository.dart';
+
+class GetTvShowRecommendations {
+  final TvShowRepository repository;
+
+  GetTvShowRecommendations(this.repository);
+
+  Future<Either<Failure, List<TvShow>>> execute(id) {
+    return repository.getTvShowRecommendations(id);
+  }
+}
