e3b3170c3cf8f8448a0f9eae787d2f08a88e9af4
Rifki <rifkipelajar@gmail.com>
2021-10-23 13:26:09 +0800

    episode tv show remote data source

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# a199-flutter-expert-project
+# Ditonton
--- lib/data/datasources/tv_show_remote_data_source.dart
+++ lib/data/datasources/tv_show_remote_data_source.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:ditonton/common/exception.dart';
+import 'package:ditonton/data/models/episode_model.dart';
+import 'package:ditonton/data/models/episode_response.dart';
 import 'package:ditonton/data/models/tv_show_detail_model.dart';
 import 'package:ditonton/data/models/tv_show_model.dart';
 import 'package:ditonton/data/models/tv_show_response.dart';
@@ -11,6 +13,7 @@ abstract class TvShowRemoteDataSource {
   Future<List<TvShowModel>> getPopularTvShows();
   Future<List<TvShowModel>> getTopRatedTvShows();
   Future<TvShowDetailResponse> getTvShowDetail(int id);
+  Future<List<EpisodeModel>> getTvShowSeasonEpisodes(int id, int seasonNumber);
   Future<List<TvShowModel>> getTvShowRecommendations(int id);
   Future<List<TvShowModel>> searchTvShows(String query);
 }
@@ -42,6 +45,18 @@ class TvShowRemoteDataSourceImpl implements TvShowRemoteDataSource {
 
     if (response.statusCode == 200) {
       return TvShowDetailResponse.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<EpisodeModel>> getTvShowSeasonEpisodes(int id, int seasonNumber) async {
+    final response = 
+      await client.get(Uri.parse('$BASE_URL/tv/$id/season/$seasonNumber?$API_KEY'));
+    
+    if(response.statusCode == 200) {
+      return EpisodeResponse.fromJson(json.decode(response.body)).episodes;
     } else {
       throw ServerException();
     }
