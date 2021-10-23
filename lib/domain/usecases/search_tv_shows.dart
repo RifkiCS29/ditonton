2b9a04841fc04b43cf3f00476fa2d68ecda0315f
Rifki <rifkipelajar@gmail.com>
2021-10-24 00:17:01 +0800

    UseCase Search Tv Shows

---
+++ lib/domain/usecases/search_tv_shows.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/repositories/tv_show_repository.dart';
+
+
+class SearchTvShows {
+  final TvShowRepository repository;
+
+  SearchTvShows(this.repository);
+
+  Future<Either<Failure, List<TvShow>>> execute(String query) {
+    return repository.searchTvShows(query);
+  }
+}
