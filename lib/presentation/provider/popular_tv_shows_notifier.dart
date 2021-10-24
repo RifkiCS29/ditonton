19ab3a80a48f46adc1941f0a5c41b79d7799b813
Rifki <rifkipelajar@gmail.com>
2021-10-24 14:48:58 +0800

    Popular Tv Show Provider

---
+++ lib/presentation/provider/popular_tv_shows_notifier.dart
@@ -0,0 +1,39 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/get_popular_tv_shows.dart';
+import 'package:flutter/foundation.dart';
+
+class PopularTvShowsNotifier extends ChangeNotifier {
+  final GetPopularTvShows getPopularTvShows;
+
+  PopularTvShowsNotifier(this.getPopularTvShows);
+
+  RequestState _state = RequestState.Empty;
+  RequestState get state => _state;
+
+  List<TvShow> _tvShows = [];
+  List<TvShow> get tvShows => _tvShows;
+
+  String _message = '';
+  String get message => _message;
+
+  Future<void> fetchPopularTvShows() async {
+    _state = RequestState.Loading;
+    notifyListeners();
+
+    final result = await getPopularTvShows.execute();
+
+    result.fold(
+      (failure) {
+        _message = failure.message;
+        _state = RequestState.Error;
+        notifyListeners();
+      },
+      (tvShowsData) {
+        _tvShows = tvShowsData;
+        _state = RequestState.Loaded;
+        notifyListeners();
+      },
+    );
+  }
+}
