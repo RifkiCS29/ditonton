b0ea9d327ddd9b7595bdf18c3fcf9ed6fb9e7ef2
Rifki <rifkipelajar@gmail.com>
2021-10-24 15:19:08 +0800

    Tv Show Search Provider

---
+++ lib/presentation/provider/tv_show_search_notifier.dart
@@ -0,0 +1,38 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/search_tv_shows.dart';
+import 'package:flutter/foundation.dart';
+
+class TvShowSearchNotifier extends ChangeNotifier {
+  final SearchTvShows searchTvShows;
+
+  TvShowSearchNotifier({required this.searchTvShows});
+
+  RequestState _state = RequestState.Empty;
+  RequestState get state => _state;
+
+  List<TvShow> _searchResult = [];
+  List<TvShow> get searchResult => _searchResult;
+
+  String _message = '';
+  String get message => _message;
+
+  Future<void> fetchTvShowSearch(String query) async {
+    _state = RequestState.Loading;
+    notifyListeners();
+
+    final result = await searchTvShows.execute(query);
+    result.fold(
+      (failure) {
+        _message = failure.message;
+        _state = RequestState.Error;
+        notifyListeners();
+      },
+      (dataTvShows) {
+        _searchResult = dataTvShows;
+        _state = RequestState.Loaded;
+        notifyListeners();
+      },
+    );
+  }
+}
