56330ddd23847671ffb11d1b62a8a3e5b8693ed0
Rifki <rifkipelajar@gmail.com>
2021-10-24 14:32:32 +0800

    Tv Show List Provider

---
+++ lib/presentation/provider/tv_show_list_notifier.dart
@@ -0,0 +1,96 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/get_airing_today_tv_shows.dart';
+import 'package:ditonton/domain/usecases/get_popular_tv_shows.dart';
+import 'package:ditonton/domain/usecases/get_top_rated_tv_shows.dart';
+import 'package:flutter/material.dart';
+
+class TvShowListNotifier extends ChangeNotifier {
+  var _airingTodayTvShows = <TvShow>[];
+  List<TvShow> get airingTodayTvShows => _airingTodayTvShows;
+
+  RequestState _airingTodayState = RequestState.Empty;
+  RequestState get airingTodayState => _airingTodayState;
+
+  var _popularTvShows = <TvShow>[];
+  List<TvShow> get popularTvShows => _popularTvShows;
+
+  RequestState _popularTvShowsState = RequestState.Empty;
+  RequestState get popularTvShowsState => _popularTvShowsState;
+
+  var _topRatedTvShows = <TvShow>[];
+  List<TvShow> get topRatedTvShows => _topRatedTvShows;
+
+  RequestState _topRatedTvShowsState = RequestState.Empty;
+  RequestState get topRatedTvShowsState => _topRatedTvShowsState;
+
+  String _message = '';
+  String get message => _message;
+
+  TvShowListNotifier({
+    required this.getAiringTodayTvShows,
+    required this.getPopularTvShows,
+    required this.getTopRatedTvShows,
+  });
+
+  final GetAiringTodayTvShows getAiringTodayTvShows;
+  final GetPopularTvShows getPopularTvShows;
+  final GetTopRatedTvShows getTopRatedTvShows;
+
+  Future<void> fetchAiringTodayTvShows() async {
+    _airingTodayState = RequestState.Loading;
+    notifyListeners();
+
+    final result = await getAiringTodayTvShows.execute();
+    result.fold(
+      (failure) {
+        _airingTodayState = RequestState.Error;
+        _message = failure.message;
+        notifyListeners();
+      },
+      (tvShowsData) {
+        _airingTodayState = RequestState.Loaded;
+        _airingTodayTvShows = tvShowsData;
+        notifyListeners();
+      },
+    );
+  }
+
+  Future<void> fetchPopularTvShows() async {
+    _popularTvShowsState = RequestState.Loading;
+    notifyListeners();
+
+    final result = await getPopularTvShows.execute();
+    result.fold(
+      (failure) {
+        _popularTvShowsState = RequestState.Error;
+        _message = failure.message;
+        notifyListeners();
+      },
+      (tvShowsData) {
+        _popularTvShowsState = RequestState.Loaded;
+        _popularTvShows = tvShowsData;
+        notifyListeners();
+      },
+    );
+  }
+
+  Future<void> fetchTopRatedTvShows() async {
+    _topRatedTvShowsState = RequestState.Loading;
+    notifyListeners();
+
+    final result = await getTopRatedTvShows.execute();
+    result.fold(
+      (failure) {
+        _topRatedTvShowsState = RequestState.Error;
+        _message = failure.message;
+        notifyListeners();
+      },
+      (tvShowsData) {
+        _topRatedTvShowsState = RequestState.Loaded;
+        _topRatedTvShows = tvShowsData;
+        notifyListeners();
+      },
+    );
+  }
+}
