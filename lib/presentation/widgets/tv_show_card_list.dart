62e5f1bd5d1348fdfce40c7d2d765266944ceb6e
Rifki <rifkipelajar@gmail.com>
2021-10-25 16:43:21 +0800

    Popular Tv Show Page

---
+++ lib/presentation/widgets/tv_show_card_list.dart
@@ -0,0 +1,74 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ditonton/common/constants.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:flutter/material.dart';
+
+class TvShowCard extends StatelessWidget {
+  final TvShow tvShow;
+
+  TvShowCard(this.tvShow);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      child: InkWell(
+        onTap: () {
+          // Navigator.pushNamed(
+          //   context,
+          //   TvShowDetailPage.ROUTE_NAME,
+          //   arguments: tvShow.id,
+          // );
+        },
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            Card(
+              child: Container(
+                margin: const EdgeInsets.only(
+                  left: 16 + 80 + 16,
+                  bottom: 8,
+                  right: 8,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tvShow.name ?? '-',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: kHeading6,
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      tvShow.overview ?? '-',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                left: 16,
+                bottom: 16,
+              ),
+              child: ClipRRect(
+                child: CachedNetworkImage(
+                  imageUrl: '$BASE_IMAGE_URL${tvShow.posterPath}',
+                  width: 80,
+                  placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
