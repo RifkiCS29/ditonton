6c3bc212c08990120d5fab127501e303c1e9a08c
Rifki <rifkipelajar@gmail.com>
2021-10-25 16:46:50 +0800

    Top Rated Tv Show Page

---
+++ lib/presentation/pages/top_rated_tv_shows_page.dart
@@ -0,0 +1,56 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/presentation/provider/top_rated_tv_shows_notifier.dart';
+import 'package:ditonton/presentation/widgets/tv_show_card_list.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class TopRatedTvShowsPage extends StatefulWidget {
+  static const ROUTE_NAME = '/top-rated-tvshow';
+
+  @override
+  _TopRatedTvShowsPageState createState() => _TopRatedTvShowsPageState();
+}
+
+class _TopRatedTvShowsPageState extends State<TopRatedTvShowsPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() =>
+        Provider.of<TopRatedTvShowsNotifier>(context, listen: false)
+            .fetchTopRatedTvShows());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top Rated Tv Shows'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Consumer<TopRatedTvShowsNotifier>(
+          builder: (context, data, child) {
+            if (data.state == RequestState.Loading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (data.state == RequestState.Loaded) {
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final tvShow = data.tvShows[index];
+                  return TvShowCard(tvShow);
+                },
+                itemCount: data.tvShows.length,
+              );
+            } else {
+              return Center(
+                key: Key('error_message'),
+                child: Text(data.message),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
