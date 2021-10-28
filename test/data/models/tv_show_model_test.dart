8f8b077630523c144b43c6e601bdbaff461322a6
Rifki <rifkipelajar@gmail.com>
2021-10-28 11:47:07 +0800

    Tv Show Model Test

---
+++ test/data/models/tv_show_model_test.dart
@@ -0,0 +1,40 @@
+import 'package:ditonton/data/models/tv_show_model.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final tTvShowModel = TvShowModel(
+      backdropPath: "/xAKMj134XHQVNHLC6rWsccLMenG.jpg",
+      firstAirDate: "2021-10-12",
+      genreIds: [10765, 35, 80],
+      id: 90462,
+      name: "Chucky",
+      originalName: "Chucky",
+      overview:
+          "After a vintage Chucky doll turns up at a suburban yard sale, an idyllic American town is thrown into chaos as a series of horrifying murders begin to expose the town’s hypocrisies and secrets. Meanwhile, the arrival of enemies — and allies — from Chucky’s past threatens to expose the truth behind the killings, as well as the demon doll’s untold origins.",
+      popularity: 6008.272,
+      posterPath: "/iF8ai2QLNiHV4anwY1TuSGZXqfN.jpg",
+      voteAverage: 8,
+      voteCount: 987
+  );
+
+  final tTvShow = TvShow(
+      backdropPath: "/xAKMj134XHQVNHLC6rWsccLMenG.jpg",
+      firstAirDate: "2021-10-12",
+      genreIds: [10765, 35, 80],
+      id: 90462,
+      name: "Chucky",
+      originalName: "Chucky",
+      overview:
+          "After a vintage Chucky doll turns up at a suburban yard sale, an idyllic American town is thrown into chaos as a series of horrifying murders begin to expose the town’s hypocrisies and secrets. Meanwhile, the arrival of enemies — and allies — from Chucky’s past threatens to expose the truth behind the killings, as well as the demon doll’s untold origins.",
+      popularity: 6008.272,
+      posterPath: "/iF8ai2QLNiHV4anwY1TuSGZXqfN.jpg",
+      voteAverage: 8,
+      voteCount: 987
+  );
+
+  test('should be a subclass of Tv Show entity', () async {
+    final result = tTvShowModel.toEntity();
+    expect(result, tTvShow);
+  });
+}
