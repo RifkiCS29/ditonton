146cfeabe69b8fd02ad0f37443990c10208e86fb
Rifki <rifkipelajar@gmail.com>
2021-10-28 12:53:00 +0800

    Tv Show Response Model Test

---
+++ test/data/models/tv_show_response_model_test.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+import 'package:ditonton/data/models/tv_show_model.dart';
+import 'package:ditonton/data/models/tv_show_response.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../json_reader.dart';
+
+void main() {
+  final tTvShowModel = TvShowModel(
+      backdropPath: "/xAKMj134XHQVNHLC6rWsccLMenG.jpg",
+      firstAirDate: "2021-10-12",
+      genreIds: [10765, 35, 80],
+      id: 90462,
+      name: "Chucky",
+      originalName: "Chucky",
+      overview:
+          "After a vintage Chucky doll turns up at a suburban yard sale, an idyllic American town is thrown into chaos as a series of horrifying murders begin to expose the town’s hypocrisies and secrets. Meanwhile, the arrival of enemies — and allies — from Chucky’s past threatens to expose the truth behind the killings, as well as the demon doll’s untold origins.",
+      popularity: 6008.272,
+      posterPath: "/iF8ai2QLNiHV4anwY1TuSGZXqfN.jpg",
+      voteAverage: 8,
+      voteCount: 987
+  );
+  final tTvShowResponseModel =
+      TvShowResponse(tvShowList: <TvShowModel>[tTvShowModel]);
+  group('fromJson', () {
+    test('should return a valid model from JSON', () async {
+      // arrange
+      final Map<String, dynamic> jsonMap =
+          json.decode(readJson('dummy_data/tv_show_popular.json'));
+      // act
+      final result = TvShowResponse.fromJson(jsonMap);
+      // assert
+      expect(result, tTvShowResponseModel);
+    });
+  });
+
+  group('toJson', () {
+    test('should return a JSON map containing proper data', () async {
+      // arrange
+
+      // act
+      final result = tTvShowResponseModel.toJson();
+      // assert
+      final expectedJsonMap = {
+        "results": [
+          {
+            "backdrop_path": "/xAKMj134XHQVNHLC6rWsccLMenG.jpg",
+            "first_air_date": "2021-10-12",
+            "genre_ids": [10765, 35, 80],
+            "id": 90462,
+            "name": "Chucky",
+            "original_name": "Chucky",
+            "overview":
+                "After a vintage Chucky doll turns up at a suburban yard sale, an idyllic American town is thrown into chaos as a series of horrifying murders begin to expose the town’s hypocrisies and secrets. Meanwhile, the arrival of enemies — and allies — from Chucky’s past threatens to expose the truth behind the killings, as well as the demon doll’s untold origins.",
+            "popularity": 6008.272,
+            "poster_path": "/iF8ai2QLNiHV4anwY1TuSGZXqfN.jpg",
+            "vote_average": 8,
+            "vote_count": 987
+          }
+        ],
+      };
+      expect(result, expectedJsonMap);
+    });
+  });
+}
