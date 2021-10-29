611aa00554ce6743b719008c13b68e46384bf3ca
Rifki <rifkipelajar@gmail.com>
2021-10-29 15:38:46 +0800

    Watchlist Tv Show Status Usecase Test

---
+++ test/domain/usecases/get_watchlist_status_tv_show_test.dart
@@ -0,0 +1,25 @@
+import 'package:ditonton/domain/usecases/get_watchlist_status_tv_show.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetWatchListStatusTvShow usecase;
+  late MockTvShowRepository mockTvShowRepository;
+
+  setUp(() {
+    mockTvShowRepository = MockTvShowRepository();
+    usecase = GetWatchListStatusTvShow(mockTvShowRepository);
+  });
+
+  test('should get watchlist status from repository', () async {
+    // arrange
+    when(mockTvShowRepository.isAddedToWatchlistTvShow(1))
+        .thenAnswer((_) async => true);
+    // act
+    final result = await usecase.execute(1);
+    // assert
+    expect(result, true);
+  });
+}
