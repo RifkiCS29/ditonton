2c07729788887ead4d2553a4e100c015a85f3743
Rifki <rifkipelajar@gmail.com>
2021-10-29 10:26:59 +0800

    Airing Today Tv Show Usecase Test

---
+++ test/domain/usecases/get_airing_today_tv_shows_test.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/get_airing_today_tv_shows.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetAiringTodayTvShows usecase;
+  late MockTvShowRepository mockTvShowRepository;
+
+  setUp(() {
+    mockTvShowRepository = MockTvShowRepository();
+    usecase = GetAiringTodayTvShows(mockTvShowRepository);
+  });
+
+  final tTvShows = <TvShow>[];
+
+  test('should get list of Tv Shows from the repository', () async {
+    // arrange
+    when(mockTvShowRepository.getAiringTodayTvShows())
+        .thenAnswer((_) async => Right(tTvShows));
+    // act
+    final result = await usecase.execute();
+    // assert
+    expect(result, Right(tTvShows));
+  });
+}
