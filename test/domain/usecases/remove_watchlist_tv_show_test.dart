26ad3dcba19df47ab6194d1ae2c08cf50236e620
Rifki <rifkipelajar@gmail.com>
2021-10-29 21:31:42 +0800

    Romove Watchlist Tv show UseCase Test

---
+++ test/domain/usecases/remove_watchlist_tv_show_test.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/usecases/remove_watchlist_tv_show.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../dummy_data/dummy_objects.dart';
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late RemoveWatchlistTvShow usecase;
+  late MockTvShowRepository mockTvShowRepository;
+
+  setUp(() {
+    mockTvShowRepository = MockTvShowRepository();
+    usecase = RemoveWatchlistTvShow(mockTvShowRepository);
+  });
+
+  test('should remove watchlist Tv Show from repository', () async {
+    // arrange
+    when(mockTvShowRepository.removeWatchlistTvShow(testTvShowDetail))
+        .thenAnswer((_) async => Right('Removed from watchlist'));
+    // act
+    final result = await usecase.execute(testTvShowDetail);
+    // assert
+    verify(mockTvShowRepository.removeWatchlistTvShow(testTvShowDetail));
+    expect(result, Right('Removed from watchlist'));
+  });
+}
