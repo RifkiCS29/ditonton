7159dc8ccd2133f6b39386f8836fa6ee85b1c0c2
Rifki <rifkipelajar@gmail.com>
2021-10-30 00:35:31 +0800

    Tv Show Search Provider test

---
+++ test/presentation/provider/tv_show_search_notifier_test.dart
@@ -0,0 +1,94 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/search_tv_shows.dart';
+import 'package:ditonton/presentation/provider/tv_show_search_notifier.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'tv_show_search_notifier_test.mocks.dart';
+
+@GenerateMocks([SearchTvShows])
+void main() {
+  late TvShowSearchNotifier provider;
+  late MockSearchTvShows mockSearchTvShows;
+  late int listenerCallCount;
+
+  setUp(() {
+    listenerCallCount = 0;
+    mockSearchTvShows = MockSearchTvShows();
+    provider = TvShowSearchNotifier(searchTvShows: mockSearchTvShows)
+      ..addListener(() {
+        listenerCallCount += 1;
+      });
+  });
+
+  final tTvShowModel = TvShow(
+      backdropPath: "/xAKMj134XHQVNHLC6rWsccLMenG.jpg",
+      firstAirDate: "2021-10-12",
+      genreIds: [10765, 35, 80],
+      id: 90462,
+      name: "Chucky",
+      originalName: "Chucky",
+      overview:
+          "After a vintage Chucky doll turns up at a suburban yard sale, an idyllic American town is thrown into chaos as a series of horrifying murders begin to expose the town’s hypocrisies and secrets. Meanwhile, the arrival of enemies — and allies — from Chucky’s past threatens to expose the truth behind the killings, as well as the demon doll’s untold origins.",
+      popularity: 6008.272,
+      posterPath: "/iF8ai2QLNiHV4anwY1TuSGZXqfN.jpg",
+      voteAverage: 8,
+      voteCount: 987
+  );
+  final tTvShowList = <TvShow>[tTvShowModel];
+  final tQuery = 'chucky';
+
+  group('search Tv Shows', () {
+    test('should change state to loading when usecase is called', () async {
+      // arrange
+      when(mockSearchTvShows.execute(tQuery))
+          .thenAnswer((_) async => Right(tTvShowList));
+      // act
+      provider.fetchTvShowSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.Loading);
+    });
+
+    test('should change search result data when data is gotten successfully',
+        () async {
+      // arrange
+      when(mockSearchTvShows.execute(tQuery))
+          .thenAnswer((_) async => Right(tTvShowList));
+      // act
+      await provider.fetchTvShowSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.Loaded);
+      expect(provider.searchResult, tTvShowList);
+      expect(listenerCallCount, 2);
+    });
+
+    test('should change search result data empty when data is gotten successfully',
+        () async {
+      // arrange
+      when(mockSearchTvShows.execute(tQuery))
+          .thenAnswer((_) async => Right([]));
+      // act
+      await provider.fetchTvShowSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.Empty);
+      expect(provider.searchResult, []);
+      expect(listenerCallCount, 3);
+    });
+
+    test('should return error when data is unsuccessful', () async {
+      // arrange
+      when(mockSearchTvShows.execute(tQuery))
+          .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+      // act
+      await provider.fetchTvShowSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.Error);
+      expect(provider.message, 'Server Failure');
+      expect(listenerCallCount, 2);
+    });
+  });
+}
