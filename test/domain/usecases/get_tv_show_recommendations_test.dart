56ea5058352cbe8443d96dca6dd3e368ee8011fe
Rifki <rifkipelajar@gmail.com>
2021-10-29 21:37:27 +0800

    Tv show Recommendations UseCase Test

---
+++ test/domain/usecases/get_tv_show_recommendations_test.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/get_tv_show_recommendations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetTvShowRecommendations usecase;
+  late MockTvShowRepository mockTvShowRepository;
+
+  setUp(() {
+    mockTvShowRepository = MockTvShowRepository();
+    usecase = GetTvShowRecommendations(mockTvShowRepository);
+  });
+
+  final tId = 1;
+  final tTvShows = <TvShow>[];
+
+  test('should get list of Tv Show recommendations from the repository',
+      () async {
+    // arrange
+    when(mockTvShowRepository.getTvShowRecommendations(tId))
+        .thenAnswer((_) async => Right(tTvShows));
+    // act
+    final result = await usecase.execute(tId);
+    // assert
+    expect(result, Right(tTvShows));
+  });
+}
