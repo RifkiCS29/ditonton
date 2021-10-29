046de4af89b45c243f3d05cd6b57b56c2cd1cb30
Rifki <rifkipelajar@gmail.com>
2021-10-30 00:14:59 +0800

    Tv Show List Provider test

---
+++ test/presentation/provider/tv_show_list_notifier_test.dart
@@ -0,0 +1,177 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/get_airing_today_tv_shows.dart';
+import 'package:ditonton/domain/usecases/get_popular_tv_shows.dart';
+import 'package:ditonton/domain/usecases/get_top_rated_tv_shows.dart';
+import 'package:ditonton/presentation/provider/tv_show_list_notifier.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'tv_show_list_notifier_test.mocks.dart';
+
+@GenerateMocks([GetAiringTodayTvShows, GetPopularTvShows, GetTopRatedTvShows])
+void main() {
+  late TvShowListNotifier provider;
+  late MockGetAiringTodayTvShows mockGetAiringTodayTvShows;
+  late MockGetPopularTvShows mockGetPopularTvShows;
+  late MockGetTopRatedTvShows mockGetTopRatedTvShows;
+  late int listenerCallCount;
+
+  setUp(() {
+    listenerCallCount = 0;
+    mockGetAiringTodayTvShows = MockGetAiringTodayTvShows();
+    mockGetPopularTvShows = MockGetPopularTvShows();
+    mockGetTopRatedTvShows = MockGetTopRatedTvShows();
+    provider = TvShowListNotifier(
+      getAiringTodayTvShows: mockGetAiringTodayTvShows,
+      getPopularTvShows: mockGetPopularTvShows,
+      getTopRatedTvShows: mockGetTopRatedTvShows,
+    )..addListener(() {
+        listenerCallCount += 1;
+      });
+  });
+
+  final tTvShow = TvShow(
+      backdropPath: "/xAKMj134XHQVNHLC6rWsccLMenG.jpg",
+      firstAirDate: "2021-10-12",
+      genreIds: [10765, 35, 80],
+      id: 90462,
+      name: "Chucky",
+      originalName: "Chucky",
+      overview:
+          "After a vintage Chucky doll turns up at a suburban yard sale, an idyllic American town is thrown into chaos as a series of horrifying murders begin to expose the town’s hypocrisies and secrets. Meanwhile, the arrival of enemies — and allies — from Chucky’s past threatens to expose the truth behind the killings, as well as the demon doll’s untold origins.",
+      popularity: 6008.272,
+      posterPath: "/iF8ai2QLNiHV4anwY1TuSGZXqfN.jpg",
+      voteAverage: 8,
+      voteCount: 987
+  );
+  final tTvShowList = <TvShow>[tTvShow];
+
+  group('now playing TvShows', () {
+    test('initialState should be Empty', () {
+      expect(provider.airingTodayState, equals(RequestState.Empty));
+    });
+
+    test('should get data from the usecase', () async {
+      // arrange
+      when(mockGetAiringTodayTvShows.execute())
+          .thenAnswer((_) async => Right(tTvShowList));
+      // act
+      provider.fetchAiringTodayTvShows();
+      // assert
+      verify(mockGetAiringTodayTvShows.execute());
+    });
+
+    test('should change state to Loading when usecase is called', () {
+      // arrange
+      when(mockGetAiringTodayTvShows.execute())
+          .thenAnswer((_) async => Right(tTvShowList));
+      // act
+      provider.fetchAiringTodayTvShows();
+      // assert
+      expect(provider.airingTodayState, RequestState.Loading);
+    });
+
+    test('should change TvShows when data is gotten successfully', () async {
+      // arrange
+      when(mockGetAiringTodayTvShows.execute())
+          .thenAnswer((_) async => Right(tTvShowList));
+      // act
+      await provider.fetchAiringTodayTvShows();
+      // assert
+      expect(provider.airingTodayState, RequestState.Loaded);
+      expect(provider.airingTodayTvShows, tTvShowList);
+      expect(listenerCallCount, 2);
+    });
+
+    test('should return error when data is unsuccessful', () async {
+      // arrange
+      when(mockGetAiringTodayTvShows.execute())
+          .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+      // act
+      await provider.fetchAiringTodayTvShows();
+      // assert
+      expect(provider.airingTodayState, RequestState.Error);
+      expect(provider.message, 'Server Failure');
+      expect(listenerCallCount, 2);
+    });
+  });
+
+  group('popular TvShows', () {
+    test('should change state to loading when usecase is called', () async {
+      // arrange
+      when(mockGetPopularTvShows.execute())
+          .thenAnswer((_) async => Right(tTvShowList));
+      // act
+      provider.fetchPopularTvShows();
+      // assert
+      expect(provider.popularTvShowsState, RequestState.Loading);
+      // verify(provider.setState(RequestState.Loading));
+    });
+
+    test('should change TvShows data when data is gotten successfully',
+        () async {
+      // arrange
+      when(mockGetPopularTvShows.execute())
+          .thenAnswer((_) async => Right(tTvShowList));
+      // act
+      await provider.fetchPopularTvShows();
+      // assert
+      expect(provider.popularTvShowsState, RequestState.Loaded);
+      expect(provider.popularTvShows, tTvShowList);
+      expect(listenerCallCount, 2);
+    });
+
+    test('should return error when data is unsuccessful', () async {
+      // arrange
+      when(mockGetPopularTvShows.execute())
+          .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+      // act
+      await provider.fetchPopularTvShows();
+      // assert
+      expect(provider.popularTvShowsState, RequestState.Error);
+      expect(provider.message, 'Server Failure');
+      expect(listenerCallCount, 2);
+    });
+  });
+
+  group('top rated TvShows', () {
+    test('should change state to loading when usecase is called', () async {
+      // arrange
+      when(mockGetTopRatedTvShows.execute())
+          .thenAnswer((_) async => Right(tTvShowList));
+      // act
+      provider.fetchTopRatedTvShows();
+      // assert
+      expect(provider.topRatedTvShowsState, RequestState.Loading);
+    });
+
+    test('should change TvShows data when data is gotten successfully',
+        () async {
+      // arrange
+      when(mockGetTopRatedTvShows.execute())
+          .thenAnswer((_) async => Right(tTvShowList));
+      // act
+      await provider.fetchTopRatedTvShows();
+      // assert
+      expect(provider.topRatedTvShowsState, RequestState.Loaded);
+      expect(provider.topRatedTvShows, tTvShowList);
+      expect(listenerCallCount, 2);
+    });
+
+    test('should return error when data is unsuccessful', () async {
+      // arrange
+      when(mockGetTopRatedTvShows.execute())
+          .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+      // act
+      await provider.fetchTopRatedTvShows();
+      // assert
+      expect(provider.topRatedTvShowsState, RequestState.Error);
+      expect(provider.message, 'Server Failure');
+      expect(listenerCallCount, 2);
+    });
+  });
+}
