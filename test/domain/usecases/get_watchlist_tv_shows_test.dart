4a1b0f6077217f5029d33cc9be845a18432f9381
Rifki <rifkipelajar@gmail.com>
2021-10-29 10:38:14 +0800

    Watchlist Tv Show Usecase Test

---
+++ test/domain/usecases/get_watchlist_tv_shows_test.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/usecases/get_watchlist_tv_shows.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../dummy_data/dummy_objects.dart';
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetWatchlistTvShows usecase;
+  late MockTvShowRepository mockTvShowRepository;
+
+  setUp(() {
+    mockTvShowRepository = MockTvShowRepository();
+    usecase = GetWatchlistTvShows(mockTvShowRepository);
+  });
+
+  test('should get list of Tv Shows from the repository', () async {
+    // arrange
+    when(mockTvShowRepository.getWatchlistTvShows())
+        .thenAnswer((_) async => Right(testTvShowList));
+    // act
+    final result = await usecase.execute();
+    // assert
+    expect(result, Right(testTvShowList));
+  });
+}
