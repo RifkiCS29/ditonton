c887a842b263aa61dbfcaf666b1a4fada2038aae
Rifki <rifkipelajar@gmail.com>
2021-10-29 21:40:26 +0800

    Tv show Search UseCase Test

---
+++ test/domain/usecases/search_tv_shows_test.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/search_tv_shows.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late SearchTvShows usecase;
+  late MockTvShowRepository mockTvShowRepository;
+
+  setUp(() {
+    mockTvShowRepository = MockTvShowRepository();
+    usecase = SearchTvShows(mockTvShowRepository);
+  });
+
+  final tTvShows = <TvShow>[];
+  final tQuery = 'Game of Thrones';
+
+  test('should get list of Tv Shows from the repository', () async {
+    // arrange
+    when(mockTvShowRepository.searchTvShows(tQuery))
+        .thenAnswer((_) async => Right(tTvShows));
+    // act
+    final result = await usecase.execute(tQuery);
+    // assert
+    expect(result, Right(tTvShows));
+  });
+}
