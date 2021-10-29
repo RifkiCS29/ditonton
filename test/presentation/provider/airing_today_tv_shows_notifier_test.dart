3296cf237a6cc629279fc869d7be68491593edb1
Rifki <rifkipelajar@gmail.com>
2021-10-30 00:03:55 +0800

    Airing Today Tv Show Provider test

---
+++ test/presentation/provider/airing_today_tv_shows_notifier_test.dart
@@ -0,0 +1,80 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/get_airing_today_tv_shows.dart';
+import 'package:ditonton/presentation/provider/airing_today_tv_shows_notifier.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'airing_today_tv_shows_notifier_test.mocks.dart';
+
+@GenerateMocks([GetAiringTodayTvShows])
+void main() {
+  late MockGetAiringTodayTvShows mockGetAiringTodayTvShows;
+  late AiringTodayTvShowsNotifier notifier;
+  late int listenerCallCount;
+
+  setUp(() {
+    listenerCallCount = 0;
+    mockGetAiringTodayTvShows = MockGetAiringTodayTvShows();
+    notifier = AiringTodayTvShowsNotifier(mockGetAiringTodayTvShows)
+      ..addListener(() {
+        listenerCallCount++;
+      });
+  });
+
+  final tTvShow = TvShow(
+      backdropPath: "/xAKMj134XHQVNHLC6rWsccLMenG.jpg",
+      firstAirDate: "2021-10-12",
+      genreIds: [10765, 35, 80],
+      id: 90462,
+      name: "Chucky",
+      originalName: "Chucky",
+      overview:
+          "After a vintage Chucky doll turns up at a suburban yard sale, an idyllic American town is thrown into chaos as a series of horrifying murders begin to expose the town’s hypocrisies and secrets. Meanwhile, the arrival of enemies — and allies — from Chucky’s past threatens to expose the truth behind the killings, as well as the demon doll’s untold origins.",
+      popularity: 6008.272,
+      posterPath: "/iF8ai2QLNiHV4anwY1TuSGZXqfN.jpg",
+      voteAverage: 8,
+      voteCount: 987
+  );
+
+  final tTvShowList = <TvShow>[tTvShow];
+
+  test('should change state to loading when usecase is called', () async {
+    // arrange
+    when(mockGetAiringTodayTvShows.execute())
+        .thenAnswer((_) async => Right(tTvShowList));
+    // act
+    notifier.fetchAiringTodayTvShows();
+    // assert
+    expect(notifier.state, RequestState.Loading);
+    expect(listenerCallCount, 1);
+  });
+
+  test('should change Tv Shows data when data is gotten successfully', () async {
+    // arrange
+    when(mockGetAiringTodayTvShows.execute())
+        .thenAnswer((_) async => Right(tTvShowList));
+    // act
+    await notifier.fetchAiringTodayTvShows();
+    // assert
+    expect(notifier.state, RequestState.Loaded);
+    expect(notifier.tvShows, tTvShowList);
+    expect(listenerCallCount, 2);
+  });
+
+  test('should return error when data is unsuccessful', () async {
+    // arrange
+    when(mockGetAiringTodayTvShows.execute())
+        .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+    // act
+    await notifier.fetchAiringTodayTvShows();
+    // assert
+    expect(notifier.state, RequestState.Error);
+    expect(notifier.message, 'Server Failure');
+    expect(listenerCallCount, 2);
+  });
+}
