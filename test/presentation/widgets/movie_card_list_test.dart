0d1ffb37271e1e861fbcf5ee41f74cbe5b483680
Rifki <rifkipelajar@gmail.com>
2021-10-30 12:07:28 +0800

    Movie and Tv Show Card Test

---
+++ test/presentation/widgets/movie_card_list_test.dart
@@ -0,0 +1,36 @@
+import 'package:ditonton/domain/entities/movie.dart';
+import 'package:ditonton/presentation/widgets/movie_card_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final movie = Movie(
+    adult: false,
+    backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+    genreIds: [14, 28],
+    id: 557,
+    originalTitle: 'Spider-Man',
+    overview:
+        'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+    popularity: 60.441,
+    posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+    releaseDate: '2002-05-01',
+    title: 'Spider-Man',
+    video: false,
+    voteAverage: 7.2,
+    voteCount: 13507,
+  );
+
+  group('Movie card Widget Test', () {
+    Widget _makeTestableWidget() {
+      return MaterialApp(home: Scaffold(body: MovieCard(movie)));
+    }
+
+    testWidgets('Testing if title movie shows', (WidgetTester tester) async {
+      await tester.pumpWidget(_makeTestableWidget());
+      expect(find.byType(Text), findsWidgets);
+      expect(find.byType(InkWell), findsOneWidget);
+      expect(find.byType(Card), findsOneWidget);
+    });
+  });
+}
---
+++ test/presentation/widgets/tv_show_card_list_test.dart
@@ -0,0 +1,34 @@
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/presentation/widgets/tv_show_card_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final movie = TvShow(
+      backdropPath: "/xAKMj134XHQVNHLC6rWsccLMenG.jpg",
+      firstAirDate: "2021-10-12",
+      genreIds: [10765, 35, 80],
+      id: 90462,
+      name: "Chucky",
+      originalName: "Chucky",
+      overview:
+          "After a vintage Chucky doll turns up at a suburban yard sale, an idyllic American town is thrown into chaos as a series of horrifying murders begin to expose the town’s hypocrisies and secrets. Meanwhile, the arrival of enemies — and allies — from Chucky’s past threatens to expose the truth behind the killings, as well as the demon doll’s untold origins.",
+      popularity: 6008.272,
+      posterPath: "/iF8ai2QLNiHV4anwY1TuSGZXqfN.jpg",
+      voteAverage: 8,
+      voteCount: 987
+  );
+
+  group('Tv Show card Widget Test', () {
+    Widget _makeTestableWidget() {
+      return MaterialApp(home: Scaffold(body: TvShowCard(movie)));
+    }
+
+    testWidgets('Testing if title tv show shows', (WidgetTester tester) async {
+      await tester.pumpWidget(_makeTestableWidget());
+      expect(find.byType(Text), findsWidgets);
+      expect(find.byType(InkWell), findsOneWidget);
+      expect(find.byType(Card), findsOneWidget);
+    });
+  });
+}
