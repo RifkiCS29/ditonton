68dd9a10b929df72356209609b5d32c5296c3927
Rifki <rifkipelajar@gmail.com>
2021-11-16 12:52:48 +0800

    Popular and Top Rated Movies Bloc Test

---
+++ movie/test/presentation/bloc/popular_movies_bloc/popular_movies_bloc_test.dart
@@ -0,0 +1,98 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:core/common/failure.dart';
+import 'package:core/domain/entities/movie.dart';
+import 'package:movie/domain/usecases/get_popular_movies.dart';
+import 'package:movie/presentation/bloc/popular_movies_bloc/popular_movies_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'popular_movies_bloc_test.mocks.dart';
+
+@GenerateMocks([GetPopularMovies])
+void main() {
+  late MockGetPopularMovies mockGetPopularMovies;
+  late PopularMoviesBloc popularMoviesBloc;
+
+  setUp(() {
+    mockGetPopularMovies = MockGetPopularMovies();
+    popularMoviesBloc = PopularMoviesBloc(mockGetPopularMovies);
+  });
+
+  final tMovie = Movie(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: 'releaseDate',
+    title: 'title',
+    video: false,
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final tMovieList = <Movie>[tMovie];
+
+  group('Popular Movies', () {
+    test('Initial state should be empty', () {
+      expect(popularMoviesBloc.state, PopularMoviesEmpty());
+    });
+
+    blocTest<PopularMoviesBloc, PopularMoviesState> (
+      'Should emit [PopularMoviesLoading, PopularMoviesLoaded] when data is gotten successfully',
+      build: () {
+        when(mockGetPopularMovies.execute())
+          .thenAnswer((_) async => Right(tMovieList));
+        return popularMoviesBloc;
+      },
+      act: (bloc) => bloc.add(PopularMoviesEvent()),
+      expect: () => [
+        PopularMoviesLoading(),
+        PopularMoviesLoaded(tMovieList),
+      ],
+      verify: (bloc) {
+        verify(mockGetPopularMovies.execute());
+      },
+    );
+
+    blocTest<PopularMoviesBloc, PopularMoviesState> (
+      'Should emit [PopularMoviesLoading, PopularMoviesLoaded[], PopularMoviesEmpty] when data is gotten successfully',
+      build: () {
+        when(mockGetPopularMovies.execute())
+          .thenAnswer((_) async => Right(<Movie>[]));
+        return popularMoviesBloc;
+      },
+      act: (bloc) => bloc.add(PopularMoviesEvent()),
+      expect: () => [
+        PopularMoviesLoading(),
+        PopularMoviesLoaded(<Movie>[]),
+        PopularMoviesEmpty(),
+      ],
+      verify: (bloc) {
+        verify(mockGetPopularMovies.execute());
+      },
+    );
+
+    blocTest<PopularMoviesBloc, PopularMoviesState> (
+      'Should emit [PopularMoviesLoading, PopularMoviesError] when get Failure',
+      build: () {
+        when(mockGetPopularMovies.execute())
+            .thenAnswer((_) async => Left(ServerFailure('Failed')));
+        return popularMoviesBloc;
+      },
+      act: (bloc) => bloc.add(PopularMoviesEvent()),
+      expect: () => [
+        PopularMoviesLoading(),
+        PopularMoviesError('Failed'),
+      ],
+      verify: (_) {
+        verify(mockGetPopularMovies.execute());
+      },
+    );
+  });
+}
---
+++ movie/test/presentation/bloc/top_rated_movies_bloc/top_rated_movies_bloc_test.dart
@@ -0,0 +1,98 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:core/common/failure.dart';
+import 'package:core/domain/entities/movie.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:movie/domain/usecases/get_top_rated_movies.dart';
+import 'package:movie/presentation/bloc/top_rated_movies_bloc/top_rated_movies_bloc.dart';
+
+import 'top_rated_movies_bloc_test.mocks.dart';
+
+@GenerateMocks([GetTopRatedMovies])
+void main() {
+  late MockGetTopRatedMovies mockGetTopRatedMovies;
+  late TopRatedMoviesBloc topRatedMoviesBloc;
+
+  setUp(() {
+    mockGetTopRatedMovies = MockGetTopRatedMovies();
+    topRatedMoviesBloc = TopRatedMoviesBloc(mockGetTopRatedMovies);
+  });
+
+  final tMovie = Movie(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: 'releaseDate',
+    title: 'title',
+    video: false,
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final tMovieList = <Movie>[tMovie];
+
+  group('TopRated Movies', () {
+    test('Initial state should be empty', () {
+      expect(topRatedMoviesBloc.state, TopRatedMoviesEmpty());
+    });
+
+    blocTest<TopRatedMoviesBloc, TopRatedMoviesState> (
+      'Should emit [TopRatedMoviesLoading, TopRatedMoviesLoaded] when data is gotten successfully',
+      build: () {
+        when(mockGetTopRatedMovies.execute())
+          .thenAnswer((_) async => Right(tMovieList));
+        return topRatedMoviesBloc;
+      },
+      act: (bloc) => bloc.add(TopRatedMoviesEvent()),
+      expect: () => [
+        TopRatedMoviesLoading(),
+        TopRatedMoviesLoaded(tMovieList),
+      ],
+      verify: (bloc) {
+        verify(mockGetTopRatedMovies.execute());
+      },
+    );
+
+    blocTest<TopRatedMoviesBloc, TopRatedMoviesState> (
+      'Should emit [TopRatedMoviesLoading, TopRatedMoviesLoaded[], TopRatedMoviesEmpty] when data is gotten successfully',
+      build: () {
+        when(mockGetTopRatedMovies.execute())
+          .thenAnswer((_) async => Right(<Movie>[]));
+        return topRatedMoviesBloc;
+      },
+      act: (bloc) => bloc.add(TopRatedMoviesEvent()),
+      expect: () => [
+        TopRatedMoviesLoading(),
+        TopRatedMoviesLoaded(<Movie>[]),
+        TopRatedMoviesEmpty(),
+      ],
+      verify: (bloc) {
+        verify(mockGetTopRatedMovies.execute());
+      },
+    );
+
+    blocTest<TopRatedMoviesBloc, TopRatedMoviesState> (
+      'Should emit [TopRatedMoviesLoading, TopRatedMoviesError] when get Failure',
+      build: () {
+        when(mockGetTopRatedMovies.execute())
+            .thenAnswer((_) async => Left(ServerFailure('Failed')));
+        return topRatedMoviesBloc;
+      },
+      act: (bloc) => bloc.add(TopRatedMoviesEvent()),
+      expect: () => [
+        TopRatedMoviesLoading(),
+        TopRatedMoviesError('Failed'),
+      ],
+      verify: (_) {
+        verify(mockGetTopRatedMovies.execute());
+      },
+    );
+  });
+}
