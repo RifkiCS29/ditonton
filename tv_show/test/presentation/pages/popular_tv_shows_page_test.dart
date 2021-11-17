2f6c8adabd75c71035db7e36a3fc8e1aff66cf95
Rifki <rifkipelajar@gmail.com>
2021-11-18 01:34:38 +0800

    Popular Tv Show Page Test

--- tv_show/test/presentation/pages/popular_tv_shows_page_test.dart
+++ tv_show/test/presentation/pages/popular_tv_shows_page_test.dart
@@ -1,26 +1,38 @@
-import 'package:core/common/state_enum.dart';
-import 'package:core/domain/entities/tv_show.dart';
-import 'package:tv_show/presentation/pages/popular_tv_shows_page.dart';
-import 'package:tv_show/presentation/provider/popular_tv_shows_notifier.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
-import 'package:provider/provider.dart';
+import 'package:tv_show/presentation/bloc/popular_tv_shows_bloc/popular_tv_shows_bloc.dart';
+import 'package:tv_show/presentation/pages/popular_tv_shows_page.dart';
 
-import 'popular_tv_shows_page_test.mocks.dart';
+import '../../dummy_data/dummy_objects.dart';
 
-@GenerateMocks([PopularTvShowsNotifier])
+class PopularTvShowsEventFake extends Fake 
+  implements PopularTvShowsEvent {}
+
+class PopularTvShowsStateFake extends Fake 
+  implements PopularTvShowsState {}
+
+class MockPopularTvShowsBloc 
+  extends MockBloc<PopularTvShowsEvent, PopularTvShowsState> 
+  implements PopularTvShowsBloc {}
+
 void main() {
-  late MockPopularTvShowsNotifier mockNotifier;
+  late MockPopularTvShowsBloc mockPopularTvShowsBloc;
+
+  setUpAll(() {
+    registerFallbackValue(PopularTvShowsEventFake());
+    registerFallbackValue(PopularTvShowsStateFake());
+  });
 
   setUp(() {
-    mockNotifier = MockPopularTvShowsNotifier();
+    mockPopularTvShowsBloc = MockPopularTvShowsBloc();
   });
 
   Widget _makeTestableWidget(Widget body) {
-    return ChangeNotifierProvider<PopularTvShowsNotifier>.value(
-      value: mockNotifier,
+    return BlocProvider<PopularTvShowsBloc>.value(
+      value: mockPopularTvShowsBloc,
       child: MaterialApp(
         home: body,
       ),
@@ -29,7 +41,7 @@ void main() {
 
   testWidgets('Page should display center progress bar when loading',
       (WidgetTester tester) async {
-    when(mockNotifier.state).thenReturn(RequestState.Loading);
+    when(() => mockPopularTvShowsBloc.state).thenReturn(PopularTvShowsLoading());
 
     final progressBarFinder = find.byType(CircularProgressIndicator);
     final centerFinder = find.byType(Center);
@@ -42,8 +54,7 @@ void main() {
 
   testWidgets('Page should display ListView when data is loaded',
       (WidgetTester tester) async {
-    when(mockNotifier.state).thenReturn(RequestState.Loaded);
-    when(mockNotifier.tvShows).thenReturn(<TvShow>[]);
+    when(() => mockPopularTvShowsBloc.state).thenReturn(PopularTvShowsLoaded([testTvShow]));
 
     final listViewFinder = find.byType(ListView);
 
@@ -52,10 +63,20 @@ void main() {
     expect(listViewFinder, findsOneWidget);
   });
 
+  testWidgets('Page should display text with message when Empty',
+      (WidgetTester tester) async {
+    when(() => mockPopularTvShowsBloc.state).thenReturn(PopularTvShowsEmpty());
+
+    final textFinder = find.text('Empty Popular Tv Show');
+
+    await tester.pumpWidget(_makeTestableWidget(PopularTvShowsPage()));
+
+    expect(textFinder, findsOneWidget);
+  });
+
   testWidgets('Page should display text with message when Error',
       (WidgetTester tester) async {
-    when(mockNotifier.state).thenReturn(RequestState.Error);
-    when(mockNotifier.message).thenReturn('Error message');
+    when(() => mockPopularTvShowsBloc.state).thenReturn(PopularTvShowsError('Failed'));
 
     final textFinder = find.byKey(Key('error_message'));
 
